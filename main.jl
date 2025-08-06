727d385ea41a5e1909927e107757ca6b132d804b
Greg Neustroev <G.Neustroev@tudelft.nl>
2025-08-06 12:29:52 +0200

    Change input file name to match the dir

--- main.jl
+++ main.jl
@@ -14,16 +14,15 @@ using Random
 n_random_seeds = 5
 Random.seed!(123)
 seeds = rand(1:1000, n_random_seeds)
-input_dir = "tyndp2024"
-experiments_file = "experiments.csv"
+input = "tyndp2024"
 
 @info "Reading experiment configuration"
 base_dir = joinpath(dirname(@__FILE__), "inputs")
-run_data = joinpath(base_dir, experiments_file) |> RunData
+run_data = joinpath(base_dir, "$(input).csv") |> RunData
 
 @info "Reading data shared across all experiments"
 connection = DBInterface.connect(DuckDB.DB, ":memory:")
-read_data_from_dir(connection, joinpath(base_dir, input_dir))
+read_data_from_dir(connection, joinpath(base_dir, input))
 
 for seed in seeds
     for run_data_row in run_data |> eachrow
